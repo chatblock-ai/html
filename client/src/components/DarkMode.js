379bb3f5c9b90f25eb1393d9f15ebc77d877eb01
dreamup1105 <highdevskills123@gmail.com>
2023-03-02 04:22:14 +0900

    update sidebar

--- client/src/components/DarkMode.js
+++ client/src/components/DarkMode.js
@@ -20,16 +20,16 @@ const DarkMode = (props) => {
         {darkTheme ? (
           <>
             <div className="nav__icons">
-              <MdOutlineWbSunny />
+              <img src="/images/light-mode.png" className="w-[25px]" />
             </div>
-            <h1 className={`${!props.open && "hidden"}`}>Light mode</h1>
+            <h1 className={`font-vt ${!props.open && "hidden"}`}>Light mode</h1>
           </>
         ) : (
           <>
             <div className="nav__icons">
-              <MdOutlineNightlight />
+            <img src="/images/dark-mode.png" className="w-[25px]" />
             </div>
-            <h1 className={`${!props.open && "hidden"}`}>Night mode</h1>
+            <h1 className={`font-vt ${!props.open && "hidden"}`}>Night mode</h1>
           </>
         )}
 
