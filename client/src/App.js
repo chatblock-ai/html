53e62b51e66cb2d9cecb62b5e5372b3a53ecc330
dreamup1105 <highdevskills123@gmail.com>
2023-03-02 01:15:53 +0900

    update landing & sigin page

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,28 @@ import SignIn from './pages/SignIn'
 import { ChatContextProvider } from './context/chatContext'
 import { WalletConnectionProvider } from './context/walletProvider';
 import { useAuthState } from 'react-firebase-hooks/auth'
+import Chatblockai from './pages/chatblockai'
+import ChatInit from './pages/chatinit'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Toaster } from 'react-hot-toast'
 
 import { auth } from './firebase'
 const App = () => {
   const [user] = useAuthState(auth)
   return (
-    <ChatContextProvider>
-      <WalletConnectionProvider defaultNetwork={'bnbt'}>
-        <div>
-          {user ? <Home /> : <SignIn />}
-        </div>
-      </WalletConnectionProvider>
-    </ChatContextProvider >
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Chatblockai />} exact path="/" />
+        <Route element={<ChatInit />} exact path="/init" />
+      </Routes>
+      {/* <ChatContextProvider>
+        <WalletConnectionProvider defaultNetwork={'bnbt'}>
+          <div>
+            {user ? <Home /> : <SignIn />}
+          </div>
+        </WalletConnectionProvider>
+      </ChatContextProvider > */}
+    </BrowserRouter>
   )
 }
 
