a2498dcdeb56a1dcd63a52dd0acdf25c9787f8fe
chatblock-ai <126272533+chatblock-ai@users.noreply.github.com>
2023-03-02 23:59:46 +0000

    Delete client directory

--- client/src/utils/log.js
+++
@@ -1,6 +0,0 @@
-const isDevEnv = process.env.NODE_ENV === "development";
-
-export const dev = {
-  log: (...message) => { isDevEnv && console.log(...message) },
-  error: (...message) => { isDevEnv && console.error(...message) }
-}
