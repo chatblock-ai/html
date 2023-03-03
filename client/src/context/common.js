26b44f83d10bbe5af2a67d2dd844e391649da70b
chatblock-ai <126272533+chatblock-ai@users.noreply.github.com>
2023-03-03 00:22:55 +0000

    Delete client directory

--- client/src/context/common.js
+++
@@ -1,3 +0,0 @@
-export const DEFAULT_CONTEXT_FUNCTION = () => {
-  //
-};
--- client/src/utils/const.js
+++
@@ -1,3 +0,0 @@
-export const contractAddress = "0x20292749BF77521D720D8ed8d6441Cbb7f9064D5"
-
-export const usdtAddress = "0x337610d27c682E347C9cD60BD4b3b107C9d34dDd"
