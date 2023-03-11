b36dc876cacaef0446e56891e89e33de2c42ced0
chatblock-ai <126272533+chatblock-ai@users.noreply.github.com>
2023-03-11 00:21:41 +0000

    Update SideBar.js

--- client/src/components/SideBar.js
+++ client/src/components/SideBar.js
@@ -64,6 +64,7 @@ const SideBar = () => {
           <span className="nav__item">
             <div className="nav__icons">
               <img src="/images/social-media.png" className="w-[20px]" />
+              <a href="https://linktr.ee/chatblockai" target="_blank"/>
             </div>
             <h1 className={`font-vt ${!open && "hidden"}`}>Social Media</h1>
           </span>
